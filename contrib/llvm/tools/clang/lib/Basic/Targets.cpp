2050utopia/unix-history-repo

commit 2aae0b192db15d6e23aecaef96217b7dbc4138d1

Fix the way clang retrieves the major FreeBSD release number from the target triple, so that the __FreeBSD__ and __FreeBSD_cc_version builtin macros return the expected results.
Spotted by:	nalitoja at gmail.com


git-svn-id: svn+ssh://svn.freebsd.org/base/head@226518 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

contrib/llvm/tools/clang/lib/Basic/Targets.cpp
@@ -238,11 +238,12 @@ protected:
                             MacroBuilder &Builder) const {
     // FreeBSD defines; list based off of gcc output
 
-    // FIXME: Move version number handling to llvm::Triple.
-    llvm::StringRef Release = Triple.getOSName().substr(strlen("freebsd"), 1);
+    unsigned Release = Triple.getOSMajorVersion();
+    if (Release == 0U)
+      Release = 8U;
 
-    Builder.defineMacro("__FreeBSD__", Release);
-    Builder.defineMacro("__FreeBSD_cc_version", Release + "00001");
+    Builder.defineMacro("__FreeBSD__", Twine(Release));
+    Builder.defineMacro("__FreeBSD_cc_version", Twine(Release * 100000U + 1U));
     Builder.defineMacro("__KPRINTF_ATTRIBUTE__");
     DefineStd(Builder, "unix", Opts);
     Builder.defineMacro("__ELF__");

commit 02e83a4d0639698c850116a6746ed2017d55056f

Fix breakage introduced by r226518.
Spotted by:	tinderbox, yanefbsd at gmail.com
Pointy hat to:	dim

contrib/llvm/tools/clang/lib/Basic/Targets.cpp
@@ -242,8 +242,9 @@ protected:
     if (Release == 0U)
       Release = 8U;
 
-    Builder.defineMacro("__FreeBSD__", Twine(Release));
-    Builder.defineMacro("__FreeBSD_cc_version", Twine(Release * 100000U + 1U));
+    Builder.defineMacro("__FreeBSD__", llvm::Twine(Release));
+    Builder.defineMacro("__FreeBSD_cc_version",
+                        llvm::Twine(Release * 100000U + 1U));
     Builder.defineMacro("__KPRINTF_ATTRIBUTE__");
     DefineStd(Builder, "unix", Opts);
     Builder.defineMacro("__ELF__");